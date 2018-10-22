f61eaa32cd8c7a4fbeafb5ffe9e5e107a00aaad7
pymhd <aram808@gmail.com>
2018-10-22 11:30:00 +0300

    simpler

--- main.go
+++ main.go
@@ -1,142 +1,131 @@
 package logging
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
+	"runtime"
+	"strings"
 	"sync"
 	"time"
-	"strings"
-	"runtime"
 )
 
-var VERBOSE bool
+const (
+	Fatal   = "\033[0;41mFATAL\033[0m"
+	Error   = "\033[0;31mERROR\033[0m"
+	Warning = "\033[0;33mWARNING\033[0m"
+	Info    = "\033[0;34mINFO\033[0m"
+	Debug   = "\033[0;37mDEBUG\033[0m"
+)
+
+type logger struct {
+	mu  sync.Mutex
+	out io.Writer
+}
+
+var logging *logger
 
 func init() {
-	flag.BoolVar(&VERBOSE, "v", false, "verbose")
+	logging = new(logger)
+	logging.out = os.Stdout
 }
 
-func New(out io.Writer) *Logger {
-	return &Logger{Out: out}
+func Debugln(v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Debug)
+	fmt.Fprintln(logging.out, v...)
 }
 
-type Logger struct {
-	sync.Mutex
-	Verbose bool
-	Out     io.Writer
+func Debugf(s string, v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Debug)
+	fmt.Fprintf(logging.out, s, v...)
 }
 
-func (l *Logger) formatHeader(severity string) {
+func Infoln(v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Info)
+	fmt.Fprintln(logging.out, v...)
+}
+
+func Infof(s string, v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Info)
+	fmt.Fprintf(logging.out, s, v...)
+}
+
+func Warningln(v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Warning)
+	fmt.Fprintln(logging.out, v...)
+}
+
+func Warningf(s string, v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Warning)
+	fmt.Fprintf(logging.out, s, v...)
+}
+
+func Errorln(v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Error)
+	fmt.Fprintln(logging.out, v...)
+}
+
+func Errorf(s string, v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Error)
+	fmt.Fprintf(logging.out, s, v...)
+}
+
+func Fatalln(v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Fatal)
+	fmt.Fprintln(logging.out, v...)
+}
+
+func Fatalf(s string, v ...interface{}) {
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+
+	printHeader(Fatal)
+	fmt.Fprintf(logging.out, s, v...)
+}
+
+func SetOutput(o io.Writer) {
+	logging.out = o
+}
+
+func printHeader(s string) {
 	// glog mostly
 	now := time.Now().Format(time.RFC822)
-	_, file, line, ok := runtime.Caller(3)
+	_, file, line, ok := runtime.Caller(2)
 	if !ok {
 		file = "????"
 		line = 0
-	} else{
+	} else {
 		slash := strings.LastIndex(file, "/")
 		if slash >= 0 {
 			file = file[slash+1:]
 		}
 	}
-	fmt.Fprintf(l.Out, "%s  %s:%d  %s: ", now, file, line, severity)
-}
-
-func (l *Logger) Infoln(v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("\033[0;34mINFO\033[0m")
-	fmt.Fprintln(l.Out, v...)
-}
-
-func (l *Logger) Infof(format string, v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("INFO")
-	fmt.Fprintf(l.Out, format, v...)
-}
-
-func (l *Logger) Errorln(v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("\033[0;31mERROR\033[0m")
-	fmt.Fprintln(l.Out, v...)
-}
-
-func (l *Logger) Errorf(format string, v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("\033[0;31mERROR\033[0m")
-	fmt.Fprintf(l.Out, format, v...)
-}
-
-func (l *Logger) Debugln(v ...interface{}) {
-	if VERBOSE {
-		l.Lock()
-		defer l.Unlock()
-		l.formatHeader("\033[0;37mDEBUG\033[0m")
-		fmt.Fprintln(l.Out, v...)
-	}
-}
-
-func (l *Logger) Debugf(format string, v ...interface{}) {
-	if VERBOSE {
-		l.Lock()
-		defer l.Unlock()
-		l.formatHeader("\033[0;37mDEBUG\033[0m")
-		fmt.Fprintf(l.Out, format, v...)
-	}
-}
-
-func (l *Logger) Warningln(v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("WARNING")
-	fmt.Fprintln(l.Out, v...)
-}
-
-func (l *Logger) Warningf(format string, v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("WARNING")
-	fmt.Fprintf(l.Out, format, v...)
-}
-
-func (l *Logger) Fatalf(format string, v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("FATAL")
-	fmt.Fprintf(l.Out, format, v...)
-	fmt.Fprintln(l.Out, "Exit with status 1")
-	os.Exit(1)
-}
-
-func (l *Logger) Fatalln(v ...interface{}) {
-	l.Lock()
-	defer l.Unlock()
-	l.formatHeader("FATAL")
-	fmt.Fprintln(l.Out, v...)
-	fmt.Fprintln(l.Out, "Exit with status 1")
-	os.Exit(1)
-}
-
-func (l *Logger) Debug(v ...interface{}) {
-	l.Debugln(v...)
-}
-
-func (l *Logger) Info(v ...interface{}) {
-	l.Infoln(v...)
-}
-
-func (l *Logger) Warning(v ...interface{}) {
-	l.Warningln(v...)
-}
-
-func (l *Logger) Error(v ...interface{}) {
-	l.Errorln(v...)
-}
-
-func (l *Logger) Fatal(v ...interface{}) {
-	l.Fatalln(v...)
+	fmt.Fprintf(logging.out, "%s  %s:%d  %s: ", now, file, line, s)
 }
